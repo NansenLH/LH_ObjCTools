a0f6698e383f229b1c9d37816aefe3c47d49d077
NansenLH <nansen_lu@163.com>
2022-11-09 01:58:26 +0800

    init framework

---
+++ LH_ObjCTools.framework/Headers/LH_ObjCTools.h
@@ -0,0 +1,21 @@
+//
+//  LH_ObjCTools.h
+//  LH_ObjCTools
+//
+//  Created by Nansen on 2022/11/9.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LH_ObjCTools.
+FOUNDATION_EXPORT double LH_ObjCToolsVersionNumber;
+
+//! Project version string for LH_ObjCTools.
+FOUNDATION_EXPORT const unsigned char LH_ObjCToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LH_ObjCTools/PublicHeader.h>
+
+#import <LH_ObjCTools/LH_UITools.h>
+
+
+
---
+++ LH_ObjCTools.framework/Headers/LH_UITools.h
@@ -0,0 +1,18 @@
+//
+//  LH_UITools.h
+//  LH_ObjCTools
+//
+//  Created by Nansen on 2022/11/9.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LH_UITools : NSObject
+
++ (NSString *)version;
+
+@end
+
+NS_ASSUME_NONNULL_END
